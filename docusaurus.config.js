c69f715293a8908aa7a9c3c39e1dfc7588b011fb
heinhoang <bachdaonhan@gmail.com>
2021-12-10 07:05:38 +0700

    config for github pages

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,15 +6,15 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'Tutsnips',
+  tagline: 'Tutsnips',
   url: 'https://your-docusaurus-test-site.com',
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'mytuts', // Usually your GitHub org/user name.
+  projectName: 'tutsnips', // Usually your repo name.
 
   presets: [
     [
